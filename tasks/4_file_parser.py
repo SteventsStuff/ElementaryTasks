79d885d6dbb9d4a13797cbd588df9b2a874c677f
vova <miroshnichenkovladimir3@gmail.com>
2019-03-25 13:40:34 +0200

    function name changed

--- tasks/4_file_parser.py
+++ tasks/4_file_parser.py
@@ -28,7 +28,7 @@ def parse_file(file_name, arguments, mode):
 
 
 if __name__ == "__main__":
-    args = set_args_task4()
+    args = set_opts_task4()
     print(args)
 
     if len(args) >= 2 and pathlib.Path(args[0]).is_file():
