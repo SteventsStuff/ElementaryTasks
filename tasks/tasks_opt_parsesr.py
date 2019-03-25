1331ba2207e5267cf4869d26dd8f6a786e238d43
vova <miroshnichenkovladimir3@gmail.com>
2019-03-25 13:40:14 +0200

    fixed docstrings

--- tasks/tasks_opt_parsesr.py
+++ tasks/tasks_opt_parsesr.py
@@ -1,46 +1,17 @@
-"""Module for parsing options and arguments for elementary tasks.
-
-Has function for create general options or args for every task and
-set of functions for every task.
-
-!!!FOR NOW: IF OPTION HAS NOT PARAMETER PROGRAM STOPS!!!
-
-Funcs:
-     create_general_parser(usage: str)
-     set_opts_task1(): add options: (-W --width, -H --height)
-     set_opts_task2(): add options: (-A -B -C -D)
-     set_opts_task3(): no other options
-
-     set_args_task4(): args: <file_name>,
-                             <find_str>,
-                             <replace_str> (optional)
-
-     set_opts_task5()
-     set_opts_task6(): add options: (-f --file, -m --method)
-     set_opts_task7()
-     set_opts_task8()
-     set_opts_task9()
-"""
+"""Module for parsing options and arguments for elementary tasks."""
 
 import optparse
 import random
 
 
 def create_general_parser(usage="%prog [options]"):
-    """
-    Create options for help information about:
-    - version
-    - author
-    """
+    """Create options for help information (version, author)"""
     version = "%prog 0.1 (No Unit Tests)"
-    general_parser = optparse.OptionParser(version=version,
-                                           usage=usage)
+    general_parser = optparse.OptionParser(version=version, usage=usage)
     general_parser.add_option("-a", "--author",
                               help="get information about author",
-                              action="store_true",
-                              default=False,
+                              action="store_true", default=False,
                               dest="author")
-
     return general_parser
 
 
@@ -53,41 +24,28 @@ def print_author(options, task_num):
 
 
 def set_opts_task1():
-    """
-    Add task 1 options:
-    - set width (-W --width)
-    - set height (-H --height)
-
-    :return: tuple of options.width,
-                      options.height
+    """Add task 1 options.
+    :return: tuple (width, height)
     """
     usage = """usage: %prog [options]
        %prog [no-options: interactive mode]"""
     task_parser = create_general_parser(usage)
 
     task_parser.add_option("-W", "--width",
-                           dest="width",
+                           default="0", dest="width",
                            help="set board width")
     task_parser.add_option("-H", "--height",
-                           dest="height",
+                           default="0", dest="height",
                            help="set board height")
 
     options = task_parser.parse_args()[0]
     print_author(options, 1)
-
     return options.width, options.height
 
 
 def set_opts_task2():
-    """
-    Add task 2 options:
-    - set side A size (-A)
-    - set side B size (-B)
-    - set side C size (-C)
-    - set side D size (-D)
-
-    :return: tuple of options.sideA, options.sideB,
-                      options.sideC, options.sideD,
+    """Add task 2 options.
+    :return: tuple (sideA, sideB, sideC, sideD)
     """
     usage = """usage: %prog [options]
        %prog [no-options: interactive mode]"""
@@ -105,12 +63,7 @@ def set_opts_task2():
 
 
 def set_opts_task3():
-    """
-    Add task 3 options:
-
-    Has no other options.
-    Only set usage and check for author option
-    """
+    """Has no other options."""
     usage = """%prog [no-options: interactive mode]"""
     task_parser = create_general_parser(usage)
 
@@ -118,43 +71,30 @@ def set_opts_task3():
     print_author(options, 3)
 
 
-def set_args_task4():
-    """
-    Add task 4 options:
-
-    Options:
-        Has no other options.
-        Set usage and check for author option.
-
-    Args:
-        <file_name>
-        <find_str>
-        <replace_str> (optional)
+def set_opts_task4():
+    """Has no other options.
+    :return: tuple of args (file_name find_str [optional: replace_str])
     """
     usage = """usage: %prog [args: <file_name> <find_str> <replace_str>]"""
     task_parser = create_general_parser(usage)
 
     options, args = task_parser.parse_args()
     print_author(options, 4)
-
     return args
 
 
 def set_args_task6():
-    """
-    Add task 6 options:
-    - set file with tickets name
-    - set method
-
-    call generate_tickets() to generate N tickets
+    """Add task 6 options.
+    :return: file_name
     """
     usage = """usage: %prog [options]"""
     task_parser = create_general_parser(usage)
     task_parser.add_option("-f", "--file",
-                           help="file with tickets [name]",
+                           help="file with tickets",
+                           default="tickets.txt",
                            dest="file_name")
     task_parser.add_option("-m", "--method",
-                           help="set method [default: easy] | hard | mixed",
+                           help="set method easy | hard [default: easy]",
                            dest="method",
                            default="easy")
 
@@ -165,13 +105,15 @@ def set_args_task6():
     return options.file_name
 
 
-def generate_tickets(file_name, method="easy", amount=500):
-    """generate N tickets with easy | hard | mixed methods"""
+def generate_tickets(file_name, method, amount=500):
+    """generate N tickets with easy | hard methods"""
     f = open(file_name, "w")
     if method == "easy":
         f.writelines("easy\n")
     elif method == "hard":
         f.writelines("hard\n")
+    else:
+        f.writelines("easy hard\n")
 
     for i in range(amount):
         val = str(random.randint(100000, 999999))
