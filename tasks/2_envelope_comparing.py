c3c26e8e9c52987ee66e1483c877f6116fd44ebc
Volodymyr <miroshnichenkovladimir3@gmail.com>
2019-03-27 00:52:20 +0200

    task 2 redone

--- tasks/2_envelope_comparing.py
+++ tasks/2_envelope_comparing.py
@@ -5,63 +5,70 @@ Task:       Task 2
 """
 
 
+def main():
+    """idk how to name this func normally"""
+    print("This program checks if it's possible to put one envelope another")
+    while True:
+        print("\nEnvelop number one: ")
+        envelope_one = create_envelope()
+        print("The first envelope has been successfully created!")
+
+        print("\nEnvelop number two: ")
+        envelope_two = create_envelope()
+        print("The second envelope has been successfully created!\n")
+
+        compare_envelopes(envelope_one, envelope_two)
+        user_choose = input("Do you want to try one more time? [Y/N]: ")
+        if user_choose.lower() == 'y' or user_choose.lower() == 'yes':
+            continue
+        else:
+            break
+
+
+def set_envelope_size():
+    while True:
+        try:
+            side_a = float(input("Enter first side: "))
+            side_b = float(input("Enter second side: "))
+            if side_a <= 0 or side_b <= 0:
+                raise ValueError
+        except (TypeError, ValueError):
+            print("You need to enter positive float values, dummy!\n")
+            continue
+        else:
+            return side_a, side_b
+
+
+def create_envelope():
+    size_a, size_b = set_envelope_size()
+    envelope = Envelope(size_a, size_b)
+    return envelope
+
+
+def compare_envelopes(envelope_1, envelope_2):
+    if envelope_2 < envelope_1 or envelope_1 < envelope_2:
+        print("You can put one envelope in another!")
+    else:
+        print("You can NOT put one envelope in another!")
+
+
 class Envelope:
     def __init__(self, side_a, side_b):
         """This class creates envelope with size: A*B
         and provides possibility for comparing two envelope.
         """
-        self.IM_flag = False
-        while True:
-            try:
-                if self.IM_flag:
-                    self.__side_a = float(input("Enter first side: "))
-                    self.__side_b = float(input("Enter second side: "))
-                else:
-                    self.__side_a = float(side_a)
-                    self.__side_b = float(side_b)
-
-                if not (self.__side_a > 0 and self.__side_b > 0):
-                    raise ValueError
-
-            except (TypeError, ValueError):
-                print("(You need to enter positive float values)")
-                self.IM_flag = True
-            else:
-                break
+        self.side_a = side_a
+        self.side_b = side_b
 
     def __lt__(self, other):
         """Method for comparing two objects.
         :return: True if self object is bigger or False otherwise
         """
-        if self.__side_a < other.__side_a and self.__side_b < other.__side_b:
+        if self.side_a < other.side_a and self.side_b < other.side_b:
             return True
         else:
             return False
 
 
 if __name__ == "__main__":
-    print("This program checks if it's possible to put one envelope another")
-
-    while True:
-        print("\nEnvelop number one: ")
-        my_envelope_one = Envelope(side_a=input("Enter first side: "),
-                                   side_b=input("Enter second side: "))
-        print("The first envelope has been successfully created!")
-
-        print("\nEnvelop number two: ")
-        my_envelope_two = Envelope(side_a=input("Enter first side: "),
-                                   side_b=input("Enter second side: "))
-        print("The second envelope has been successfully created!\n")
-
-        if my_envelope_two < my_envelope_one:
-            print("You can put one envelope in another")
-        elif my_envelope_one < my_envelope_two:
-            print("You can put one envelope in another")
-        else:
-            print("You can NOT put one envelope in another")
-
-        user_choose = input("Do you want to try one more time? [Y/N]: ")
-        if user_choose.lower() == 'y' or user_choose.lower() == 'yes':
-            continue
-        else:
-            break
+    main()
